df13d0288b52d659571aa3eff07f342e2f9cff5b
coderedsamurai <mishr.akasha1@gmail.com>
2024-11-01 13:59:26 +0530

    comment db

--- app/api/dashboard/route.js
+++ app/api/dashboard/route.js
@@ -2,13 +2,13 @@ import { NextResponse } from "next/server";
 import connect from "../../../lib/db";
 import Product from "../../../lib/models/product";
 
-export const GET = async (request) => {
+export const GET = async () => {
   try {
     await connect();
     const products = await Product.find();
-    return new NextResponse(products.json());
+    return new NextResponse(JSON.stringify(products), { status: 200 });
   } catch (error) {
-    return new NextResponse({ err: error, req: request }, { status: 500 });
+    return new NextResponse(error, { status: 500 });
   }
 };
 
